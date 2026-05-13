7e9ed062095920da217e4d2010ce9621d41b4845
SabinoGi <git.gih25@gmail.com>
2026-05-13 18:13:46 -0300

    shell ok

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -17,30 +17,44 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_swap(int *a, int *b)
+void	ft_swap(char **s1, char **s2)
 {
-	int	copy_a;
+	char	*copy;
 
-	copy_a = *a;
-	*a = *b;
-	*b = copy_a;
+	copy = *s1;
+	*s1 = *s2;
+	*s2 = copy;
 }
 
-void	ft_sort(int *ac, char **av)
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1 && *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_params(int ac, char **av)
 {
 	int	i;
-	int	j;
+	int	reord;
 
-	i = 1;
-	while (i < ac)
+	reord = 1;
+	while (reord)
 	{
-		j = 0;
-		while ((av[i][j] != av[i + 1][j]))
+		reord = 0;
+		i = 1;
+		while (i < ac - 1)
 		{
-			ft_swap(av[i][j], av[i + 1][j]);
-			j++;
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+			{
+				ft_swap(&av[i], &av[i + 1]);
+				reord = 1;
+			}
+			i++;
 		}
-		i++;
 	}
 }
 
@@ -50,11 +64,11 @@ int	main(int ac, char **av)
 	int	j;
 
 	i = 1;
+	ft_sort_params(ac, av);
 	while (i < ac)
 	{
-		ft_sort (ac, av);
 		j = 0;
-		while (av[i][j] != '\0')
+		while (av[i][j])
 		{
 			ft_putchar(av[i][j]);
 			j++;
